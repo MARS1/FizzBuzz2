4eb67fdbff68760aa802490611282335637a4c4e
Marcelo Berta <marcelo.berta@gmail.com>
2014-08-21 21:36:03 -0400

    First version of FizzBuzz

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 $(document).ready(function(){
 
-	var x;
-	var y;
-	var numbers = [];
-	var solution = $('.solutionWrapper');
-
-	var fizzBuzz = " FizzBuzz ";
-	var fizz = " Fizz ";
-	var buzz = " Buzz ";
+	var x,
+		y,
+		numbers = [],
+		solution = $('.solutionWrapper'),
+		fizzBuzz = " FizzBuzz ",
+		fizz = " Fizz ",
+		buzz = " Buzz ";
 
 	function fbFunction(i) {
 		if( i % 3 === 0 && i % 5 === 0) {
 			numbers.push( fizzBuzz + "<br/>");
 		} else if ( i % 3 === 0 ) {
 			numbers.push( fizz + " <br/>");
-			return undefined;
 		} else if ( i % 5 === 0 ) {
 			numbers.push( buzz + "<br/>");
-			return undefined;
 		} else {
 			numbers.push([i] + "<br/>");
-			return undefined;
 		}
 	};
 
 	for(i=1; i <=100; i++){
-		fbFunction([i]);
-		console.log(numbers);
-		solution.prepend(numbers);
+		fbFunction(i);
 	}
+	console.log(numbers);
+	solution.prepend(numbers);
 
 
 });
